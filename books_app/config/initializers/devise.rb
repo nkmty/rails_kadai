fd967eda19157d599e9a0c92d31b38f77acdfabf
nkmty <nakamoto.yuuki@gmail.com>
2019-01-03 18:21:28 +0900

    [add] facebook認証機能の追加

--- books_app/config/initializers/devise.rb
+++ books_app/config/initializers/devise.rb
@@ -258,6 +258,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :facebook, "534898343676944", "b5b702c1fbc203402b93b2477bce420f"
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
