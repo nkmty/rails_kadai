11d3e8edae8ebdc4a4a98f5b992847c1a58b95dc
nkmty <nakamoto.yuuki@gmail.com>
2019-01-15 18:45:36 +0900

    [modify] 不要な改行の削除

--- books_app/app/models/user.rb
+++ books_app/app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :validatable
 
-
-
   def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
