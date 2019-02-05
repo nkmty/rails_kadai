55f1988c91d6f79e4d589e4fd53ca1a7bd8eaf18
nkmty <nakamoto.yuuki@gmail.com>
2019-02-05 18:44:39 +0900

    helperのメソッドを外部に切り出しました。

--- books_app/spec/spec_helper.rb
+++ books_app/spec/spec_helper.rb
@@ -96,10 +96,3 @@ RSpec.configure do |config|
 =end
 end
 
-def regist_test_data
-    visit ('/books/new')
-    fill_in('book_title', with: 'test_title')
-    fill_in('book_memo', with: 'test_memo')
-    fill_in('book_author', with: 'test_author')
-    click_button('Create 本')
-end
