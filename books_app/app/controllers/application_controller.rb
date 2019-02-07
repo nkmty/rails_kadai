864b92d688e59ac909de5ca94def02da9017ea7b
nkmty <nakamoto.yuuki@gmail.com>
2019-02-07 19:45:52 +0900

    コードをキレイにしました。

--- books_app/app/controllers/application_controller.rb
+++ books_app/app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
     end
+
     def after_sign_in_path_for(resource)
       books_path
     end
@@ -15,5 +16,4 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource)
       books_path
     end
-
 end
--- books_app/spec/features/books_spec.rb
+++ books_app/spec/features/books_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 feature "書籍のCRUD操作" do
-
-before do
-  User.create(email: "test@gmail.com", password: "testtest")
-  visit ('/users/sign_in')
-  fill_in('user_email', with: 'test@gmail.com')
-  fill_in('user_password', with: 'testtest')
-  click_button('ログイン')
-end
+  before do
+    User.create(email: "test@gmail.com", password: "testtest")
+    visit ("/users/sign_in")
+    fill_in("user_email", with: "test@gmail.com")
+    fill_in("user_password", with: "testtest")
+    click_button("ログイン")
+  end
 
   scenario "書籍の登録に成功する" do
     regist_test_data
@@ -18,27 +17,26 @@ end
   scenario "作成した書籍データの閲覧" do
     regist_test_data
     regist_book_id = URI.parse(current_url).to_s.scan(/.$/)
-    visit('/books/'+regist_book_id[0])
+    visit("/books/" + regist_book_id[0])
     expect(page).to have_content "test_title"
   end
 
   scenario "書籍のデータを変更" do
     regist_test_data
     regist_book_id = URI.parse(current_url).to_s.scan(/.$/)
-    visit('/books/'+regist_book_id[0]+'/edit')
-    fill_in('book_title', with: 'test_edit')
-    fill_in('book_memo', with: 'test_edit')
-    fill_in('book_author', with: 'test_edit')
-    click_button('Update 本')
+    visit("/books/" + regist_book_id[0] + "/edit")
+    fill_in("book_title", with: "test_edit")
+    fill_in("book_memo", with: "test_edit")
+    fill_in("book_author", with: "test_edit")
+    click_button("Update 本")
     expect(page).to have_content "test_edit"
   end
 
   scenario "書籍のデータを削除" do
     regist_test_data
-    regist_book_id = URI.parse(current_url).to_s.scan(/.$/)
-    visit('/books/')
-    click_link('削除')
-    visit('/books/')
+    visit("/books/")
+    click_link("削除")
+    visit("/books/")
     expect(page).to have_no_content "test_title"
   end
 end
