edc98f0cb83718fa57c9165790a584ea42ec1af4
nkmty <nakamoto.yuuki@gmail.com>
2019-02-28 00:04:12 +0900

    インデントの修正

--- books_app/spec/models/books_spec.rb
+++ books_app/spec/models/books_spec.rb
@@ -1,13 +1,10 @@
-require 'rails_helper'
-
-  describe Book do
+require "rails_helper"
+describe Book do
   scenario "書籍のデータを生成する" do
-  book = Book.new(
-      title: 'test',
-      memo: 'testtest'
-    )
-
-  expect(book).to be_valid
+    book = Book.new(
+      title: "test",
+      memo: "testtest"
+     )
+    expect(book).to be_valid
   end
 end
-
